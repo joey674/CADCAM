f0131ee8dc8981e2a85cd864050bed70b813b67e
ZhouyiGuan <zhouyi.guan@zd-automotive.de>
2025-01-03 17:36:40 +0100

    feat: 0301

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import Mx from "mxdraw";
-import { drawPolyLine, drawAnyLine } from "./draw";
+import { drawPolyLine, drawAnyLine , drawLine, drawEllipse, drawCircle} from "./draw";
 
 
 function App() {
@@ -12,17 +12,58 @@ function App() {
         console.log("load MxDraw finish", mxobj, dom);
       },
     });
+
+    /* add command hint */
+    Mx.MxFun.addCommand("drawLine", () => {
+      if (Mx.MxFun.isRunningCommand()) {
+          return
+      }
+      drawLine()
+    })
+    Mx.MxFun.addCommand("drawCircle", () => {
+      if (Mx.MxFun.isRunningCommand()) {
+          return
+      }
+      drawCircle()
+    })
+    Mx.MxFun.listenForCommandLineInput(({ msCmdTip, msCmdDisplay, msCmdText }) => {
+      console.log(msCmdTip, msCmdDisplay, msCmdText)
+      document.getElementById('mxCmdArea')!.innerHTML = msCmdTip;
+      document.getElementById("mxCmdArea")!.scrollTop = document.getElementById("mxCmdArea")!.scrollHeight;
+    })
+
   }, []);
 
   return (
     <div>
       <h1>CADCAM</h1>
-
+      <button onClick={drawLine}>line</button>
       <button onClick={drawPolyLine}>polyline</button>
       <button onClick={drawAnyLine}>anyline</button>
+      <button onClick={drawCircle}>circle</button>
+      <button onClick={drawEllipse}>ellipse</button>
+      {/* <button onClick={test}>undefined</button>
+      <button onClick={test}>undefined</button>
+      <button onClick={test}>undefined</button>
+      <button onClick={test}>undefined</button>
+      <button onClick={test}>undefined</button>
+      <button onClick={test}>undefined</button>
+      <button onClick={test}>undefined</button>
+      <button onClick={test}>test</button> */}
+
 
       <div style={{ height: "80vh", overflow: "hidden" }}>
         <canvas id="mxdraw"></canvas>
+      </div>
+      <div>
+        <textarea
+          id="mxCmdArea"
+          rows={5}
+          style={{ width: "100%", overflow: "scroll" }}
+          placeholder="command:"
+          readOnly 
+        > command</textarea>
+        <input type="text" id="mxCmdText" />
       </div>
     </div>
   );
--- src/draw.ts
+++ src/draw.ts
@@ -1,6 +1,27 @@
 import Mx from "mxdraw";
 import { getSnappedPoint } from "./drawAssist";
 
+export const drawLine = async () => {
+  const getPoint = new Mx.MrxDbgUiPrPoint();
+
+  getPoint.setMessage("\nchoose start point");
+  getPoint.go(async (status: number) => {
+    if (status != Mx.MrxDbgUiPrBaseReturn.kOk) return;
+    let pt1 = getPoint.value();
+    let line = new Mx.MxDbLine();
+    line.pt1 = pt1;
+    getPoint.setMessage("\n choose end point");
+
+    getPoint.setUserDraw((pt: any, pw: any) => {
+      line.pt2 = pt;
+      pw.drawCustomEntity(line);
+    });
+    let pt2 = await getPoint.go();
+    line.pt2 = pt2!;
+    Mx.MxFun.getCurrentDraw().addMxEntity(line);
+  });
+};
+
 export const drawPolyLine = async () => {
   let line = new Mx.MxDbPolyline();
   const getPoint = new Mx.MrxDbgUiPrPoint();
@@ -41,5 +62,42 @@ export const drawAnyLine = async () => {
   Mx.MxFun.getCurrentDraw().addMxEntity(line);
 };
 
-export const drawEllipse = async () => {};
-export const drawCircle = async () => {};
+export const drawEllipse = async () => {
+  const getPoint = new Mx.MrxDbgUiPrPoint();
+  const ellipse = new Mx.MxDbEllipse();
+  const pt1 = await getPoint.go();
+  if (!pt1) return;
+  ellipse.point1 = pt1;
+  getPoint.setUserDraw((currentPoint, worldDraw) => {
+    ellipse.point2 = currentPoint;
+    worldDraw.drawCustomEntity(ellipse);
+  });
+  // 获取下一次鼠标点击的位置
+  const pt2 = await getPoint.go();
+  if (!pt2) return;
+  ellipse.point2 = pt2;
+  const mxDraw = Mx.MxFun.getCurrentDraw();
+  mxDraw.addMxEntity(ellipse);
+};
+
+export const drawCircle = async () => {
+  const getPoint = new Mx.MrxDbgUiPrPoint();
+  const mxObj = Mx.MxFun.getCurrentDraw();
+  const obj = new Mx.MxDbCircleShape();
+
+  getPoint.setMessage("\nchoose center point");
+  const center = await getPoint.go();
+  if (!center) return;
+  obj.center = center;
+  getPoint.setUserDraw((currentPoint, worldDraw) => {
+    obj.innerRadius = currentPoint;
+    worldDraw.drawCircle(obj.center, obj.center.distanceTo(currentPoint));
+  });
+
+  getPoint.setMessage("\nchoose circle edge point");
+  const pt = await getPoint.go();
+  if (!pt) return;
+  obj.xRadius = obj.yRadius = obj.center.distanceTo(pt);
+  obj.isClosedToCenter = false;
+  mxObj.addMxEntity(obj);
+};
